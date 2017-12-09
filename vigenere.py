22724df489939eb51cbca159686f823994b84668
Petru-Armand Bancila <petrubancila@gmail.com>
2017-12-09 02:49:19 +0200

    Add vigenere cipher

---
+++ vigenere.py
@@ -0,0 +1,11 @@
+def caesar(text, shift):
+    cipherText = list(map(lambda x: chr((ord(x) - 97 + shift) % 26 + 97), text.lower()))
+    return ''.join(cipherText)
+
+def vigenere(text, password):
+    cipherText = list(map(lambda i: caesar(text[i], ord(password[i % len(password)].lower()) - 97), range(len(text))))
+    return ''.join(cipherText)
+
+
+print(vigenere('textt', 'oink'))
+
